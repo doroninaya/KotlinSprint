89701ff8bcc4a7015fc12203da2bf9e7e12ae7a0
yanaschis <doronina.yana@gmail.com>
2025-02-26 22:15:06 +0300

    KS-1-1 Making variables for orders

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,12 @@
 package org.example.lesson_1
 
 fun main(){
-    println("Тестовый текст")
+// Объявление переменных и присваивание значений, принудительно указала тип,
+// Значения переменных не меняла, поэтому val
+    val numberOfOrders: Int = 75
+    val thanksgivingTexst: String = "Thank you for your order ;)"
+
+//Вывод значения переменных
+    println("Число сделанных заказов: " + numberOfOrders) //вывод значения переменных
+    println("Благодарственный текст: " + thanksgivingTexst)
 }
