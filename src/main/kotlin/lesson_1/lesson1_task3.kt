bf72895ceff4d2413d428b3e8e04543348816852
yanaschis <doronina.yana@gmail.com>
2025-02-27 21:29:40 +0300

    KS-1-4 Making variables String for year and time

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,9 +2,9 @@ package org.example.lesson_1
 
 fun main(){
 
-    val year: Int = 1961 //не меняем значения переменной VAL
-    var hour: Int = 9   //меняем значение VAR
-    var minute: Int = 7 //меняем значение VAR
+    val year: String = "1961" //не меняем значения переменной VAL
+    var hour: String = "09"   //меняем значение VAR
+    var minute: String = "07" //меняем значение VAR
 
     //Выведи год, час и минуту взлета по одной переменной на каждую строку
     println("Год взлета: "+year)
@@ -12,8 +12,8 @@ fun main(){
     println("Минута: "+minute)
 
     //Затем измени значения переменных hour и minute, чтобы они теперь имели значение часа и минуты посадки
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     //Выведи время посадки в одной строке
     print("Время посадки: "+hour+":"+minute)
