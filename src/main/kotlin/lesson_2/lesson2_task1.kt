7e3260c28c1e56ef059ccaa182462af0288333b1
yanaschis <doronina.yana@gmail.com>
2025-02-28 00:09:13 +0300

    KS-2-1 Сalculating the average value

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+    val markOfFirstStudent = 3
+    val markOfSecondStudent = 4
+    val markOfThirdStudent = 3
+    val markOfFourthStudent = 5
+    val average: Float
+
+    average = (markOfFirstStudent+markOfSecondStudent+markOfThirdStudent+markOfFourthStudent)/4f
+
+    println(average)
+}
