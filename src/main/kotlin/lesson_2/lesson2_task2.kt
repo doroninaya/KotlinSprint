e23af2e091794ee244d4f8b8ad5c4aad7f26e7eb
yanaschis <doronina.yana@gmail.com>
2025-03-04 22:34:33 +0300

    KS-2-2 Calculation of salaries

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_2
+
+fun main() {
+    val numberOfEmployees = 50
+    val salaryOfEmployees = 30000
+    val numberOfInterns = 30
+    val salaryOfInterns = 20000
+
+    val salaryExpensesForEmployees = numberOfEmployees * salaryOfEmployees
+    println("Расходы на выплату зарплаты постоянных сотрудников: " + salaryExpensesForEmployees + " рублей.")
+
+    val commonSalaryExpenses = salaryExpensesForEmployees + numberOfInterns * salaryOfInterns
+    println("Общие расходы по ЗП после прихода стажеров: " + commonSalaryExpenses + " рублей.")
+
+    val averageSalary = commonSalaryExpenses / (numberOfEmployees + numberOfInterns)
+    println("Средняя ЗП одного сотрудника после устройства стажеров: " + averageSalary + " рублей.")
+}
