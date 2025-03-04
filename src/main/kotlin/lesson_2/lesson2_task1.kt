b3a50d7d50f2c655589de78de9a7565b82190467
yanaschis <doronina.yana@gmail.com>
2025-03-04 17:44:38 +0300

    KS-2-1 Fixing the calculation

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
-    val markOfFirstStudent = 3
+    /*val markOfFirstStudent = 3
     val markOfSecondStudent = 4
     val markOfThirdStudent = 3
     val markOfFourthStudent = 5
-    val average: Float
 
-    average = (markOfFirstStudent+markOfSecondStudent+markOfThirdStudent+markOfFourthStudent)/4f
+    var average = (markOfFirstStudent + markOfSecondStudent + markOfThirdStudent + markOfFourthStudent)/ 4f)*/
 
-    println(average)
+    val marks = listOf(3, 4, 3, 5)
+    val newAverage = marks.average()
+    println("Average value is " + newAverage)
 }
