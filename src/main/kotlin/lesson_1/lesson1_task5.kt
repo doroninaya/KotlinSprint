839b619a2dc48b962ee4244a64181fc8b91207a6
yanaschis <doronina.yana@gmail.com>
2025-03-05 15:41:34 +0300

    KS-1-5 Fixing "magic numbers" and using "String.format"

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,37 +1,16 @@
 package org.example.lesson_1
 
-import kotlin.math.abs
+const val NUMBER_OF_SECONDS_IN_HOUR = 3600
+const val NUMBER_OF_SECONDS_IN_MINUTE = 60
 
-fun main(){
-    //время взлета
-    var hour = 9
-    var minute = 7
-
-    var timeSpentInSpace = hour*3600 + minute*60
-    //println(timeSpentInSpace)
-
-    //Затем измени значения переменных hour и minute, чтобы они теперь имели значение часа и минуты посадки
-    hour = 10
-    minute = 55
-    timeSpentInSpace = abs(timeSpentInSpace - (hour*3600 + minute*60)) //время проведенное в космосе
-
-    println(timeSpentInSpace)
-
+fun main() {
+    val seconds = 6480 // проведенное в космосе в секундах
     // Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
-    val hoursInSpace = timeSpentInSpace/3600
-    val minutesInSpace = (timeSpentInSpace - hoursInSpace*3600)/60
-    val secondsInSpace = timeSpentInSpace - hoursInSpace*3600 - minutesInSpace*60
-    //println(hoursInSpace)
-    //print(minutesInSpace)
-    //println(secondsInSpace)
+    val hoursInSpace = seconds / NUMBER_OF_SECONDS_IN_HOUR
+    val minutesInSpace = (seconds % NUMBER_OF_SECONDS_IN_HOUR) / NUMBER_OF_SECONDS_IN_MINUTE
+    val secondsInSpace = (seconds % NUMBER_OF_SECONDS_IN_HOUR) % NUMBER_OF_SECONDS_IN_MINUTE
 
     // Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
-    // Значения часов, минут и секунд отображать двумя цифрами.
-    print("0")
-    print(hoursInSpace)
-    print(":")
-    print(minutesInSpace)
-    print(":")
-    print("0")
-    print(secondsInSpace)
+    val formattedTime = String.format("%02d:%02d:%02d", hoursInSpace, minutesInSpace, secondsInSpace)
+    println("Время, проведенное в космосе $formattedTime")
 }
