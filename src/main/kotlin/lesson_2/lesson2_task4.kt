678df9cac05720dfc6ea16fd3c82e57c28233f2f
yanaschis <doronina.yana@gmail.com>
2025-03-05 17:56:58 +0300

    KS-2-4 Fixing variables

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,12 @@ package org.example.lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val Buf = 0.2
-    val bufCrystalOre = (Buf * crystalOre).toInt()
-    val bufIronOre = (Buf * ironOre).toInt()
+    val bonusPercentage = 20 // Проценты
+    // Преобразование процента в десятичное значение (20% = 0.2)
+    val bonusFactor = bonusPercentage / 100.0
+
+    val bufCrystalOre = (bonusFactor * crystalOre).toInt()
+    val bufIronOre = (bonusFactor * ironOre).toInt()
 
     println("Koличество бонусной кристаллической руды: $bufCrystalOre")
     println("Koличество бонусной железной руды: $bufIronOre")
