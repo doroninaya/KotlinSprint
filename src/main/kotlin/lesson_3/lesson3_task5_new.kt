2b5ecb360dd25968c3411d2ff810df13ed5ebf39
yanaschis <doronina.yana@gmail.com>
2025-03-06 12:49:50 +0300

    KS-3-5 Spliting chess move again

--- src/main/kotlin/lesson_3/lesson3_task5_new.kt
+++ src/main/kotlin/lesson_3/lesson3_task5_new.kt
@@ -1,8 +1,8 @@
 package org.example.lesson_3
 
 fun main() {
-    val phrase = "D2-D4;0"
-    val spliting = phrase.split("-",";")
+    val phrase = "D2-D4;0"//фраза на вход
+    val spliting = phrase.split("-", ";")
     val whereFrom = spliting[0]
     val where = spliting[1]
     val moveNumber = spliting[2].toInt()
